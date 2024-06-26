6f93f5a8a6ac4c4ae262c35d42b940baeeb13c8d
seydbasil <seyd.mb@accubits.com>
2024-06-26 19:55:43 +0530

    hilt DI basic setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,20 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
     namespace = "com.smbvt.bst.cleanarchitectureroomdatabaseflowexample"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.smbvt.bst.cleanarchitectureroomdatabaseflowexample"
         minSdk = 25
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -30,11 +34,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -66,4 +70,13 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // hilt
+    implementation("com.google.dagger:hilt-android:2.47")
+    kapt("com.google.dagger:hilt-android-compiler:2.44.2")
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
 }
---
+++ app/src/main/java/com/smbvt/bst/cleanarchitectureroomdatabaseflowexample/App.kt
@@ -0,0 +1,9 @@
+package com.smbvt.bst.cleanarchitectureroomdatabaseflowexample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+
+}
